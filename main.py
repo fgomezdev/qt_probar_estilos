6b942c5741c98e3a1322854aeab96fb554c3154c
Fernando D. Gomez <fgomezdev@gmail.com>
2021-10-26 20:34:43 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from PyQt5.QtWidgets import (QApplication, QDialog)
+
+from archivos_ui.frm_prueba_estilo_ui import Ui_frmPruebaEstilo
+
+
+class frmModelo(QDialog, Ui_frmPruebaEstilo):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+
+    import sys
+    app = QApplication(sys.argv)
+    qss="styles.qss"
+    with open(qss,"r") as fh:
+        app.setStyleSheet(fh.read())
+    example = frmModelo()
+    example.show()
+    sys.exit(app.exec_()) 
+
+
+#Ejecutar este comando dentro del directorio 'archivos_ui'
+# pyuic5 -x frm_prueba_estilo.ui -o frm_prueba_estilo_ui.py
